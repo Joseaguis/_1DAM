c6ee51bd8a26208574e316377969ea4fe2dad19a
Jose Manuel Aguilar <joseaguis1@hotmail.com>
2022-01-18 15:01:59 +0100

    18/01/2022

--- Pro. 5 Excepciones/src/_03Ejercicios/_04SerieDePeguntas.java
+++ Pro. 5 Excepciones/src/_03Ejercicios/_04SerieDePeguntas.java
@@ -4,8 +4,37 @@ public class _04SerieDePeguntas {
 	/*
 	 * 4.	Valor al final del try: 44
 	 * 		Valor al final: 45
+	 * 		Valor al final: 46
 	 * 
-	 * 5.	
+	 * 5. 	Valor 43
+	 * 		Valor 44
+	 * 		Valor 45
+	 * 		Valor 45
 	 * 		
+	 * 6.	Valor Excepcion en metodo()
+	 * 		
+	 * 7. 	Valor 43
+	 * 		Valor 44
+	 * 		Valor 45
+	 * 		Valor 45
+	 * 
+	 * 8.a	Valor Ejecución de finally
+	 * 		Error
+	 * 
+	 * 8.b	Valor a = 0
+	 * 		Valor div por 0:
+	 * 		Valor Ejecución de finally
+	 * 		
+	 * 8.c	Valor a = 7
+	 * 		Valor Indice fuera de límites
+	 * 		Valor Ejecución de finally
+	 * 
+	 * 9.	Valor dentro del procA
+	 * 		Valor Finally del procA
+	 * 		Valor dentro del procB
+	 * 		Valor Finally del procB
+	 * 
+	 * 10.	Valor capturada en método
+	 * 
 	 */
 }
